32fcf72058cc6be3eac6c37117ecf6e5818d7368
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-03-16 17:33:20 +0530

    Fixed all code format issues.

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -2,33 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-	var play = true
+	const playButton = document.getElementById('play');
+	const pauseButton = document.getElementById('pause');
+	const restartButton = document.getElementById('restart');
+	const submitButton = document.getElementById('submit');
 
-	var playButton = document.getElementById('play');
-	var pauseButton = document.getElementById('pause');
-	var restartButton = document.getElementById('restart');
-	var submitButton = document.getElementById('submit');
-
-	pauseButton.addEventListener('click', function() { window.clearTimeout(tmHandle) });
+	pauseButton.addEventListener('click', function() { window.clearTimeout(tmHandle); });
 	playButton.addEventListener('click', function() {  window.clearTimeout(tmHandle); tmHandle = setTimeout(draw, 1000 / fps); });
-	restartButton.addEventListener('click', function() {restart()})
+	restartButton.addEventListener('click', function() {restart();});
 
 	function restart() 
 	{ 
-		window.clearTimeout(tmHandle) 
+		window.clearTimeout(tmHandle); 
 
 		bldg = [
 			[[upL[0], defY], [upR[0], defY], [startR[0], defY + height], [startL[0], defY + height]],
 			[[upL[1], defY], [upR[1], defY], [startR[1], defY + height], [startL[1], defY + height]],
 			[[upL[2], defY], [upR[2], defY], [startR[2], defY + height], [startL[2], defY + height]]
-		]
+		];
 
 		ground = [
 			[startL[0] - 50, defY + height + 40],
 			[startL[0], defY + height - 40],
 			[startR[2] + 50, defY + height - 40],
 			[startR[2], defY + height + 40],
-		]
+		];
 
 		layer2 = [
 			{...ground[0]},
@@ -37,34 +35,34 @@ document.addEventListener('DOMContentLoaded', function(){
 			[ground[2][0] + thickness, defY + height - 40 + thickness],
 			{...ground[2]},
 			{...ground[3]},
-		]
+		];
 
 		dirn = -1;
 		tmHandle = window.setTimeout(draw, 1000 / fps); 
 	}
 
-	var slider_hei = document.getElementById("height");
-	var output_hei = document.getElementById("demo_height");
+	const slider_hei = document.getElementById("height");
+	const output_hei = document.getElementById("demo_height");
 	output_hei.innerHTML = slider_hei.value; // Display the default slider value
 
 	// Update the current slider value (each time you drag the slider handle)
 	slider_hei.oninput = function() {
 		output_hei.innerHTML = this.value;
-	}
+	};
 
-	var slider_mot = document.getElementById("motion");
-	var output_mot = document.getElementById("demo_motion");
+	const slider_mot = document.getElementById("motion");
+	const output_mot = document.getElementById("demo_motion");
 	output_mot.innerHTML = slider_mot.value; // Display the default slider value
 
 	// Update the current slider value (each time you drag the slider handle)
 	slider_mot.oninput = function() {
 		output_mot.innerHTML = this.value;
-	}
+	};
 
 	submitButton.addEventListener('click', function() {
-		height = Number(document.getElementById("height").value)
-		vibe = Number(document.getElementById("motion").value)
-		restart()
+		height = Number(document.getElementById("height").value);
+		vibe = Number(document.getElementById("motion").value);
+		restart();
 	});
 
 	function curvedArea(ctx, e, gradX, gradY)
@@ -75,72 +73,75 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	function updateGround(ground, layer2, chg)
 	{
-		for(let i = 0; i < ground.length; ++i)
-			ground[i][0] += chg
-		for(let i = 0; i < layer2.length; ++i)
-			layer2[i][0] += chg
+		ground.forEach(g => {
+			g[0] += chg;
+		});
+
+		layer2.forEach(l => {
+			l[0] += chg;
+		});
 	}
 
 	function drawGround(ctx, ground)
 	{
-		ctx.save()
+		ctx.save();
 		ctx.fillStyle = "pink";
 		ctx.beginPath();
 		ctx.moveTo(ground[0][0], ground[0][1]);
 
 		for(let i = 0; i < ground.length; ++i)
 		{
-			let next = (i + 1) % ground.length
-			ctx.lineTo(ground[next][0], ground[next][1])
+			let next = (i + 1) % ground.length;
+			ctx.lineTo(ground[next][0], ground[next][1]);
 		}
 
 		ctx.closePath();
 		ctx.fill();
 		ctx.stroke();
-		ctx.restore()
+		ctx.restore();
 	}
 
-	let height = 400
-	let vibe = 30
+	let height = 400;
+	let vibe = 30;
 
 	const canvas = document.getElementById("main");
 	canvas.width = 1200;
 	canvas.height = 600;
-	canvas.style = "border:3px solid"
+	canvas.style = "border:3px solid";
 	const ctx = canvas.getContext("2d");
 
-	fill = "#A9A9A9"
-	border = "black"
-	lineWidth = 1.5
+	fill = "#A9A9A9";
+	border = "black";
+	lineWidth = 1.5;
 
-	const fps = 15
-	let dirn = -1
-	let scale = 5
+	const fps = 15;
+	let dirn = -1;
+	let scale = 5;
 
-	let defY = 100
-	let startL = [120, 370, 770]
-	let startR = [270, 670, 1070]
+	let defY = 100;
+	let startL = [120, 370, 770];
+	let startR = [270, 670, 1070];
 
-	let upL = {...startL}
-	let upR = {...startR}
-	upL[1] = startL[1] + (startR[1] - startL[1]) / 3
-	upR[1] = startR[1] - (startR[1] - startL[1]) / 3
-	upL[2] = startL[2] + (startR[2] - startL[2]) / 3
-	upR[2] = startR[2] - (startR[2] - startL[2]) / 3
-	let thickness = 10
+	let upL = {...startL};
+	let upR = {...startR};
+	upL[1] = startL[1] + (startR[1] - startL[1]) / 3;
+	upR[1] = startR[1] - (startR[1] - startL[1]) / 3;
+	upL[2] = startL[2] + (startR[2] - startL[2]) / 3;
+	upR[2] = startR[2] - (startR[2] - startL[2]) / 3;
+	let thickness = 10;
 
 	let bldg = [
 		[[upL[0], defY], [upR[0], defY], [startR[0], defY + height], [startL[0], defY + height]],
 		[[upL[1], defY], [upR[1], defY], [startR[1], defY + height], [startL[1], defY + height]],
 		[[upL[2], defY], [upR[2], defY], [startR[2], defY + height], [startL[2], defY + height]]
-	]
+	];
 
 	let ground = [
 		[startL[0] - 50, defY + height + 40],
 		[startL[0], defY + height - 40],
 		[startR[2] + 50, defY + height - 40],
 		[startR[2], defY + height + 40],
-	]
+	];
 	
 	let layer2 = [
 		{...ground[0]},
@@ -149,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function(){
 		[ground[2][0] + thickness, defY + height - 40 + thickness],
 		{...ground[2]},
 		{...ground[3]},
-	]
+	];
 
 	function draw()
 	{
@@ -160,77 +161,88 @@ document.addEventListener('DOMContentLoaded', function(){
 		ctx.lineJoin = "round";
 
 		if(dirn == -1)
-			updateGround(ground, layer2, vibe / scale)
-		else
-			updateGround(ground, layer2, -1 * vibe / scale)
+		{
+			updateGround(ground, layer2, vibe / scale);
+		}
 
-		drawGround(ctx, ground)
-		drawGround(ctx, layer2)
+		else
+		{
+			updateGround(ground, layer2, -1 * vibe / scale);
+		}
+
+		drawGround(ctx, ground);
+		drawGround(ctx, layer2);
 
 		for(let k = 0; k < 3; ++k)
 		{
-			let v = bldg[k]
+			let v = bldg[k];
 
 			if(dirn == -1)
 			{
-				v[0][0] -= vibe / scale
-				v[1][0] -= vibe / scale
-				v[2][0] += vibe / scale
-				v[3][0] += vibe / scale
+				v[0][0] -= vibe / scale;
+				v[1][0] -= vibe / scale;
+				v[2][0] += vibe / scale;
+				v[3][0] += vibe / scale;
 			}
 
 			else
 			{
-				v[0][0] += vibe / scale
-				v[1][0] += vibe / scale
-				v[2][0] -= vibe / scale
-				v[3][0] -= vibe / scale
+				v[0][0] += vibe / scale;
+				v[1][0] += vibe / scale;
+				v[2][0] -= vibe / scale;
+				v[3][0] -= vibe / scale;
 			}
 
 			if(k == 2 && (v[0][0] <= upL[k] - vibe || v[1][0] >= upR[k] + vibe))
-				dirn *= -1
+			{
+				dirn *= -1;
+			}
 
 			ctx.beginPath();
 			ctx.moveTo(v[0][0], v[0][1]);
 
 			for(let i = 0; i < v.length; ++i)
 			{
-				let next = (i + 1) % v.length
-				let ctrl = v[next]
-				let ratio = 0.475
-				let ind = i
-				let e1 = [...v[i]]
-				let e2 = [...v[next]]
-				let gradX = (e1[0] - e2[0]) / -4
-				let gradY = 10
+				let next = (i + 1) % v.length;
+				let ctrl = v[next];
+				let ratio = 0.475;
+				let ind = i;
+				let e1 = [...v[i]];
+				let e2 = [...v[next]];
+				let gradX = (e1[0] - e2[0]) / -4;
+				let gradY = 10;
 
 				if(i == 0 || i == 2)
 				{
-					curvedArea(ctx, e1, gradX, gradY)
-					continue
+					curvedArea(ctx, e1, gradX, gradY);
+					continue;
 				}
 
 				if(k == 1)
-					ratio = 0.5
+				{
+					ratio = 0.5;
+				}
 
 				if(k == 0 && i == 1)
 				{
-					ratio = 1 - ratio
-					ind = next
+					ratio = 1 - ratio;
+					ind = next;
 				}
 
 				if((k == 1 || k == 2) && i == 3)
 				{
-					ratio = 1 - ratio
-					ind = next
+					ratio = 1 - ratio;
+					ind = next;
 				}
 
 				if(i == 1 || i == 3)
 				{
-					ctrl = [v[ind][0], (v[i][1] + v[next][1]) * ratio]
+					ctrl = [v[ind][0], (v[i][1] + v[next][1]) * ratio];
 					if(k)
-						ctrl = [(v[next][0] + v[i][0]) * ratio, (v[i][1] + v[next][1]) * ratio]
-
+					{
+						ctrl = [(v[next][0] + v[i][0]) * ratio, (v[i][1] + v[next][1]) * ratio];
+						
+					}
 				}
 
 				ctx.quadraticCurveTo(ctrl[0], ctrl[1], v[next][0], v[next][1]);
@@ -241,28 +253,28 @@ document.addEventListener('DOMContentLoaded', function(){
 			ctx.stroke();
 
 			// upper curved area
-			ctx.save()
+			ctx.save();
 			ctx.fillStyle = "white";
-			e1 = [...v[1]]
-			e2 = [...v[0]]
-			gradX = (e1[0] - e2[0]) / -4
-			gradY = 10
+			e1 = [...v[1]];
+			e2 = [...v[0]];
+			gradX = (e1[0] - e2[0]) / -4;
+			gradY = 10;
 
 			ctx.beginPath();
 			ctx.moveTo(e2[0], e2[1]);
-			curvedArea(ctx, e2, -1 * gradX, -1 * gradY)
-			curvedArea(ctx, e1, gradX, gradY)
+			curvedArea(ctx, e2, -1 * gradX, -1 * gradY);
+			curvedArea(ctx, e1, gradX, gradY);
 			ctx.closePath();
 
 			ctx.fill();
 			ctx.stroke();
 			ctx.restore();
 
-			bldg[k] = v
+			bldg[k] = v;
 		}
 
 		tmHandle = window.setTimeout(draw, 1000 / fps);
 	}
 
-	var tmHandle = window.setTimeout(draw, 1000 / fps);
+	let tmHandle = window.setTimeout(draw, 1000 / fps);
 })
