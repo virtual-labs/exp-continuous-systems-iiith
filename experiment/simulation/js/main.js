1b6d146648510348c41f7c1695edb8bbc7707065
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-04-12 17:29:19 +0530

    Code review fixes.

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function(){
+document.addEventListener('DOMContentLoaded', function(){
 
 	const playButton = document.getElementById('play');
 	const pauseButton = document.getElementById('pause');
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', async function(){
 
 	pauseButton.addEventListener('click', function() { window.clearTimeout(tmHandle); });
 	playButton.addEventListener('click', function() {  window.clearTimeout(tmHandle); tmHandle = setTimeout(draw, 1000 / fps); });
-	restartButton.addEventListener('click', async function() {await restart();});
+	restartButton.addEventListener('click', function() {restart();});
 
-	async function restart() 
-	{ 
-		window.clearTimeout(tmHandle); 
+	const dataSets = [data1, data2, data3, data4, data5, data6];
 
+	function init()
+	{
 		bldg = [
 			[[upL[0], defY], [upR[0], defY], [startR[0], defY + height], [startL[0], defY + height]],
 			[[upL[1], defY], [upR[1], defY], [startR[1], defY + height], [startL[1], defY + height]],
@@ -34,14 +34,16 @@ document.addEventListener('DOMContentLoaded', async function(){
 			{...ground[3]},
 		];
 
-		const file = `./data/groundData${dataNum}.json`;
-		await fetch(file)
-			.then(response => response.json())
-			.then(jsonResponse => {data = {...jsonResponse}});
-
+		data = dataSets[dataNum - 1];
 		keys = Object.keys(data);
 		ind = 0;
 		prev = keys.length - 1;
+	}
+
+	function restart() 
+	{ 
+		window.clearTimeout(tmHandle); 
+		init();
 		tmHandle = window.setTimeout(draw, 1000 / fps); 
 	}
 
@@ -49,26 +51,115 @@ document.addEventListener('DOMContentLoaded', async function(){
 	const output_hei = document.getElementById("demo_height");
 	output_hei.innerHTML = slider_hei.value; // Display the default slider value
 
-	slider_hei.oninput = async function() {
+	slider_hei.oninput = function() {
 		output_hei.innerHTML = this.value;
 		height = Number(document.getElementById("height").value);
-		await restart();
+		restart();
 	};
 
 	const slider_mot = document.getElementById("motion");
 	const output_mot = document.getElementById("demo_motion");
 	output_mot.innerHTML = slider_mot.value; // Display the default slider value
 
-	slider_mot.oninput = async function() {
+	slider_mot.oninput = function() {
 		output_mot.innerHTML = this.value;
 		dataNum = Number(document.getElementById("motion").value);
-		await restart();
+		restart();
 	};
+
+	function setToData(ground, layer2, bldg, flag)
+	{
+		//flag = 1 => reset values to default
+		if(flag)
+			scale *= -1;
+
+		ground.forEach(g => {
+			g[0] += scale * data[keys[ind]];
+		});
+
+		layer2.forEach(l => {
+			l[0] += scale * data[keys[ind]];
+		});
+
+		bldg.forEach(v => {
+			if(ind < 100)
+			{
+				v[0][0] += scale * -1 * data[keys[prev]];
+				v[1][0] += scale * -1 * data[keys[prev]];
+			}
+
+			else
+			{
+				v[0][0] += scale * 2 * data[keys[prev]];
+				v[1][0] += scale * 2 * data[keys[prev]];
+			}
+
+			v[2][0] += scale * data[keys[ind]];
+			v[3][0] += scale * data[keys[ind]];
+		});
+
+		if(flag)
+			scale *= -1;
+	}
 
 	function curvedArea(ctx, e, gradX, gradY)
 	{
 		ctx.bezierCurveTo(e[0], e[1] += gradY, e[0] += gradX, e[1] += gradY, e[0] += gradX, e[1]);
 		ctx.bezierCurveTo(e[0] += gradX, e[1], e[0] += gradX, e[1] -= gradY, e[0], e[1] -= gradY);
+	}
+
+	function drawBldg(ctx, v, k)
+	{
+		for(let i = 0; i < v.length; ++i)
+		{
+			const next = (i + 1) % v.length;
+			let ctrl = v[next];
+			let ratio = 0.475;
+			let index = i;
+			const e1 = [...v[i]];
+			const e2 = [...v[next]];
+			const gradX = (e1[0] - e2[0]) / -4;
+			const gradY = 10;
+
+			if(i === 0 || i === 2)
+			{
+				curvedArea(ctx, e1, gradX, gradY);
+				continue;
+			}
+
+			if(k === 1)
+			{
+				ratio = 0.5;
+			}
+
+			if(k === 0 && i === 1)
+			{
+				ratio = 1 - ratio;
+				index = next;
+			}
+
+			if((k === 1 || k === 2) && i === 3)
+			{
+				ratio = 1 - ratio;
+				index = next;
+			}
+
+			if(i === 1 || i === 3)
+			{
+				ctrl = [v[index][0], (v[i][1] + v[next][1]) * ratio];
+				if(k)
+				{
+					ctrl = [(v[next][0] + v[i][0]) * ratio, (v[i][1] + v[next][1]) * ratio];
+
+				}
+			}
+
+			ctx.quadraticCurveTo(ctrl[0], ctrl[1], v[next][0], v[next][1]);
+		}
+
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
 	}
 
 	function drawGround(ctx, ground)
@@ -92,12 +183,7 @@ document.addEventListener('DOMContentLoaded', async function(){
 
 	let height = 400;
 	let dataNum = 1;
-
 	let data = [];
-	const file = `./data/groundData${dataNum}.json`;
-	await fetch(file)
-		.then(response => response.json())
-		.then(jsonResponse => {data = {...jsonResponse}});
 
 	const canvas = document.getElementById("main");
 	canvas.width = 1200;
@@ -109,7 +195,7 @@ document.addEventListener('DOMContentLoaded', async function(){
 	const lineWidth = 1.5;
 
 	const fps = 10;
-	const scale = 25;
+	let scale = 25;
 
 	const defY = 100;
 	const startL = [120, 370, 770];
@@ -123,31 +209,8 @@ document.addEventListener('DOMContentLoaded', async function(){
 	upR[2] = startR[2] - (startR[2] - startL[2]) / 3;
 	const thickness = 10;
 
-	let bldg = [
-		[[upL[0], defY], [upR[0], defY], [startR[0], defY + height], [startL[0], defY + height]],
-		[[upL[1], defY], [upR[1], defY], [startR[1], defY + height], [startL[1], defY + height]],
-		[[upL[2], defY], [upR[2], defY], [startR[2], defY + height], [startL[2], defY + height]]
-	];
-
-	let ground = [
-		[startL[0] - 50, defY + height + 40],
-		[startL[0], defY + height - 40],
-		[startR[2] + 50, defY + height - 40],
-		[startR[2], defY + height + 40],
-	];
-	
-	let layer2 = [
-		{...ground[0]},
-		[ground[0][0], defY + height + 40 + thickness],
-		[startR[2] + thickness, defY + height + 40 + thickness],
-		[ground[2][0] + thickness, defY + height - 40 + thickness],
-		{...ground[2]},
-		{...ground[3]},
-	];
-
-	let keys = Object.keys(data);
-	let ind = 0;
-	let prev = keys.length - 1;
+	let bldg, ground, layer2, keys, ind, prev; 
+	init();
 
 	function draw()
 	{
@@ -157,94 +220,18 @@ document.addEventListener('DOMContentLoaded', async function(){
 		ctx.lineCap = "round";
 		ctx.lineJoin = "round";
 
-		ground.forEach(g => {
-			g[0] += scale * data[keys[ind]];
-		});
-		layer2.forEach(l => {
-			l[0] += scale * data[keys[ind]];
-		});
-
+		setToData(ground, layer2, bldg, 0);
 		drawGround(ctx, ground);
 		drawGround(ctx, layer2);
-
-		ground.forEach(g => {
-			g[0] -= scale * data[keys[ind]];
-		});
-		layer2.forEach(l => {
-			l[0] -= scale * data[keys[ind]];
-		});
 
 		for(let k = 0; k < 3; ++k)
 		{
 			let v = bldg[k];
-			if(ind < 100)
-			{
-				v[0][0] += scale * -1 * data[keys[prev]];
-				v[1][0] += scale * -1 * data[keys[prev]];
-			}
-			else
-			{
-				v[0][0] += scale * 2 * data[keys[prev]];
-				v[1][0] += scale * 2 * data[keys[prev]];
-			}
-
-			v[2][0] += scale * data[keys[ind]];
-			v[3][0] += scale * data[keys[ind]];
 
 			ctx.beginPath();
 			ctx.moveTo(v[0][0], v[0][1]);
 
-			for(let i = 0; i < v.length; ++i)
-			{
-				const next = (i + 1) % v.length;
-				let ctrl = v[next];
-				let ratio = 0.475;
-				let index = i;
-				const e1 = [...v[i]];
-				const e2 = [...v[next]];
-				const gradX = (e1[0] - e2[0]) / -4;
-				const gradY = 10;
-
-				if(i === 0 || i === 2)
-				{
-					curvedArea(ctx, e1, gradX, gradY);
-					continue;
-				}
-
-				if(k === 1)
-				{
-					ratio = 0.5;
-				}
-
-				if(k === 0 && i === 1)
-				{
-					ratio = 1 - ratio;
-					index = next;
-				}
-
-				if((k === 1 || k === 2) && i === 3)
-				{
-					ratio = 1 - ratio;
-					index = next;
-				}
-
-				if(i === 1 || i === 3)
-				{
-					ctrl = [v[index][0], (v[i][1] + v[next][1]) * ratio];
-					if(k)
-					{
-						ctrl = [(v[next][0] + v[i][0]) * ratio, (v[i][1] + v[next][1]) * ratio];
-						
-					}
-				}
-
-				ctx.quadraticCurveTo(ctrl[0], ctrl[1], v[next][0], v[next][1]);
-			}
-
-			ctx.closePath();
-			ctx.fill();
-			ctx.stroke();
-
+			drawBldg(ctx, v, k);
 			// upper curved area
 			ctx.save();
 			ctx.fillStyle = "white";
@@ -263,22 +250,10 @@ document.addEventListener('DOMContentLoaded', async function(){
 			ctx.stroke();
 			ctx.restore();
 
-			if(ind < 100)
-			{
-				v[0][0] -= scale * -1 * data[keys[prev]];
-				v[1][0] -= scale * -1 * data[keys[prev]];
-			}
-			else
-			{
-				v[0][0] -= scale * 2 * data[keys[prev]];
-				v[1][0] -= scale * 2 * data[keys[prev]];
-			}
-
-			v[2][0] -= scale * data[keys[ind]];
-			v[3][0] -= scale * data[keys[ind]];
 			bldg[k] = v;
 		}
 
+		setToData(ground, layer2, bldg, 1);
 		ind = (ind + 1) % keys.length;
 		prev = (prev + 1) % keys.length;
 		tmHandle = window.setTimeout(draw, 1000 / fps);
